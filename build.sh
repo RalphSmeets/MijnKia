e92ed2013024d4d8df1ef10fe6d2c6e3dd06368f
Ralph Smeets <rsmeets-ext@degiro.nl>
2022-07-13 15:24:59 +0200

    - Refactored the code to become compliant with latest API from KIA - Improved the helm charts around it!

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/sh -
-docker login https://hub.docker.com/v2/repositories/ralphsmeets -u ralphsmeets -p VFzT0fnzdh
+docker login -u ralphsmeets -p VFzT0fnzdh
 docker buildx create --name mybuilder
 docker buildx use mybuilder
 docker buildx inspect --bootstrap
 
 docker buildx build \
   --platform linux/amd64,linux/arm/v7,linux/arm64 \
-  -t mijn-kia-app:1.0.0 \
+  -t ralphsmeets/mijn-kia-app:1.17.0 \
+  -t ralphsmeets/mijn-kia-app:latest \
   --push \
   .
